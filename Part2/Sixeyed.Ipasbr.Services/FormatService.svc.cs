9b17c1fb23f4caa8685d32aa105c9fddc49efb65
Elton Stoneman <es@sixeyed.com>
2012-09-26 22:01:42 +0100

    Added Part2

---
+++ Part2/Sixeyed.Ipasbr.Services/FormatService.svc.cs
@@ -0,0 +1,12 @@
+﻿using System.Linq;
+
+namespace Sixeyed.Ipasbr.Services
+{
+    public class FormatService : IFormatService
+    {
+        public string ReverseString(string input)
+        {
+            return new string(input.Reverse().ToArray());
+        }
+    }
+}
---
+++ Part2/Sixeyed.Ipasbr.Services/TargetNamespace.cs
@@ -0,0 +1,8 @@
+﻿
+namespace Sixeyed.Ipasbr.Services
+{
+    public struct TargetNamespace
+    {
+        public const string SixeyedIpasbr = "com.sixeyed.ipasbr";
+    }
+}
