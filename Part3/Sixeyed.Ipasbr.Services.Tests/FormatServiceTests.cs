44352030deb2b3c7199056c5aafa388607d97a2c
Elton Stoneman <es@sixeyed.com>
2012-10-04 22:05:41 +0100

    Added Part 3

---
+++ Part3/Sixeyed.Ipasbr.Services.Tests/FormatServiceTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Net;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Sixeyed.Ipasbr.Services.Tests
+{
+    [TestClass]
+    public class FormatServiceTests
+    {
+        [TestMethod]
+        public void ReverseString_Part1_REST()
+        {
+            var input = Guid.NewGuid().ToString();
+            var expected = new string(input.Reverse().ToArray());
+            var url = string.Format("http://localhost/Sixeyed.Ipasbr.Services/FormatService.svc/rest/reverse?string={0}", input);
+            var client = new WebClient();
+            var actual = client.DownloadString(url).Replace("\"", string.Empty);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
